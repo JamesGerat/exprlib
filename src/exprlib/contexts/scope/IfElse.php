8b8346440ca7d8330938ffe467161c1aaaa585d4
Gorev Kirill <gorev@starline.ru>
2023-11-07 19:51:59 +0300

    Change if evaluate for fix like this "if(1=1,1,0/0)"

--- src/exprlib/contexts/scope/IfElse.php
+++ src/exprlib/contexts/scope/IfElse.php
@@ -9,11 +9,14 @@ class IfElse extends ScopeGroup
 {
     public function evaluate()
     {
-        $result = parent::evaluate();
-        if (!is_array($result) || count($result) !== 3) {
+        if (!empty($this->operations)) {
+            $this->addScopeGroup($this->operations);
+        }
+
+        if (!is_array($this->scopeGroups) || count($this->scopeGroups) !== 3) {
             throw new ParsingException('If must have 3 arguments, ex: if(a > 2, 1, 2)');
         }
 
-        return $result[0] ? $result[1] : $result[2];
+        return $this->scopeGroups[0]->evaluate() ? $this->scopeGroups[1]->evaluate() : $this->scopeGroups[2]->evaluate();
     }
 }
